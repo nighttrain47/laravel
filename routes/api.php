2dae9399a917e1b93ecef1acc101b5595e5820d0
ffoxkawaii <tranguyenx@gmail.com>
2024-11-14 10:57:46 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+
+// day la route de goi controller
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/search', [BookController::class, 'search']);
